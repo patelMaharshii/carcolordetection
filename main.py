2e8483bb5250a3f6ed3354a58e854f38cb37a1de
Maharshii Patel <maharshiip13@gmail.com>
2025-05-24 19:47:50 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Importing libraries
+from transparent_background import Remover 
+import yaml
+import torch
+import numpy as np
+from PIL import Image
+
+# remover = Remover()
+img = Image.open("trystuff.jpg").convert("RGB")
+print(img)
+
+# Running of model
+def main():
+    print("Running the model...")
+    
+
+# __name__
+if __name__=="__main__":
+    main()
